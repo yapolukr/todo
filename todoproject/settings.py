012e1abe22f960a23a559428e365b31da6b1c57a
yapolukr <106332725+yapolukr@users.noreply.github.com>
2022-09-27 13:21:47 +0200

    installed heroku

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -9,10 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.template.backends import django
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -121,5 +124,8 @@ STATIC_URL = 'static/'
 LOGIN_URL = '/login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
